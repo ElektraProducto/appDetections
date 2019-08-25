e5a4b7e26c45e7608f1757a671bb646721b9ed9b
Coral Rodriguez <coralrodriguez@rappi.com>
2019-08-25 06:22:36 -0500

    app detect

---
+++ app/src/main/java/elektra/erik/com/MainActivity.kt
@@ -0,0 +1,112 @@
+package elektra.erik.com
+
+import ai.fritz.core.Fritz
+import ai.fritz.fritzvisionobjectmodel.FritzVisionObjectPredictor
+import ai.fritz.vision.inputs.FritzVisionImage
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.renderscript.Allocation
+import android.renderscript.Element
+import android.renderscript.RenderScript
+import android.renderscript.ScriptIntrinsicYuvToRGB
+import android.util.Log
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var renderScript: RenderScript
+    private lateinit var yuvToRGB: ScriptIntrinsicYuvToRGB
+    private var yuvDataLength: Int = 0
+    private lateinit var allocationIn: Allocation
+    private lateinit var allocationOut: Allocation
+    private lateinit var bitmapOut: Bitmap
+    private lateinit var database: DatabaseReference
+    private val itemMap by lazy {
+        hashMapOf<String, Int>()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Fritz.configure(this)
+
+        val objectPredictor = FritzVisionObjectPredictor.getInstance(this)
+        var fritzVisionImage: FritzVisionImage
+
+        database = FirebaseDatabase.getInstance().getReference("users")
+        val query = database.orderByChild("full_name").equalTo("Memo Vera")
+
+        cameraView.addFrameProcessor {
+
+            if (yuvDataLength == 0) {
+                initializeData()
+            }
+
+            allocationIn.copyFrom(it.data)
+            yuvToRGB.forEach(allocationOut)
+            allocationOut.copyTo(bitmapOut)
+            fritzVisionImage = FritzVisionImage.fromBitmap(bitmapOut, it.rotation)
+            val visionObjects = objectPredictor.predict(fritzVisionImage)
+
+            itemMap.clear()
+
+            visionObjects.forEach { visionObject ->
+                if (itemMap.containsKey(visionObject.visionLabel.text))
+                    itemMap[visionObject.visionLabel.text] = itemMap[visionObject.visionLabel.text]!! + 1
+                itemMap[visionObject.visionLabel.text] = 1
+            }
+
+            runOnUiThread {
+                tvDetectedItem.text = ""
+                itemMap.forEach { map ->
+                    tvDetectedItem.append("Detected ${map.value} ${map.key}\n")
+                    query.addListenerForSingleValueEvent(object : ValueEventListener {
+                        override fun onDataChange(dataSnapshot: DataSnapshot) {
+                            var isTech = 0
+                            val children = dataSnapshot.children
+                            if (map.key.equals("cell phone")) {
+                                isTech = 1
+                            }
+                            children.forEach {
+                                Log.d("data", it.toString())
+                                database.child(it.key.toString()).child("is_tech").setValue(isTech)
+                                Log.d("data", it.child("is_tech").value.toString())
+                            }
+
+                        }
+
+                        override fun onCancelled(databaseError: DatabaseError) {
+
+                        }
+                    })
+                }
+            }
+        }
+    }
+
+    private fun initializeData() {
+        yuvDataLength = cameraView.previewSize?.height!! * cameraView.previewSize?.width!! * 3 / 2
+        renderScript = RenderScript.create(baseContext)
+        yuvToRGB = ScriptIntrinsicYuvToRGB.create(renderScript, Element.U8_4(renderScript))
+        allocationIn = Allocation.createSized(renderScript, Element.U8(renderScript), yuvDataLength)
+        bitmapOut = Bitmap.createBitmap(
+            cameraView.previewSize?.width!!,
+            cameraView.previewSize?.height!!,
+            Bitmap.Config.ARGB_8888
+        )
+        allocationOut = Allocation.createFromBitmap(renderScript, bitmapOut)
+        yuvToRGB.setInput(allocationIn)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        cameraView.start()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        cameraView.stop()
+    }
+}
